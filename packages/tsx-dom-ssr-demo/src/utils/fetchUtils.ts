2e57f35ac7c8ff86a71b0257b702f7f18c8cc92e
Santo Pfingsten <core@lusito.info>
2022-05-25 22:52:42 +0200

    fetch issues fixed

--- packages/tsx-dom-ssr-demo/src/utils/fetchUtils.ts
+++ packages/tsx-dom-ssr-demo/src/utils/fetchUtils.ts
@@ -1,6 +1,19 @@
 import NodeCache from "node-cache";
 
-const jsonCache = new NodeCache({
+type StatusListener<T> = { resolve: (value: T) => void; reject: (value: unknown) => void };
+type StatusProgress<T> = { status: "progress"; listeners: Array<StatusListener<T>> };
+type StatusSuccess<T> = { status: "success"; value: T };
+type StatusError = { status: "error"; error: unknown };
+type CacheEntry<T> = StatusProgress<T> | StatusSuccess<T> | StatusError;
+
+const statusProgress = <T>(listeners: Array<StatusListener<T>>): StatusProgress<T> => ({
+    status: "progress",
+    listeners,
+});
+const statusSuccess = <T>(value: T): StatusSuccess<T> => ({ status: "success", value });
+const statusError = (error: unknown): StatusError => ({ status: "error", error });
+
+const cache = new NodeCache({
     stdTTL: 5 * 60, // 5 Minutes
     checkperiod: 60,
 });
@@ -12,14 +25,31 @@ async function fetchJsonUncached(url: string) {
     return response.json();
 }
 
-export function fetchJson<T>(url: string) {
-    let promise: Promise<T> | undefined = jsonCache.get(url);
-    if (promise === undefined) {
-        promise = fetchJsonUncached(url);
-        jsonCache.set(url, promise);
+export async function fetchJson<T>(url: string): Promise<T> {
+    const entry: CacheEntry<T> | undefined = cache.get(url);
+    if (entry) {
+        if (entry.status === "success") return entry.value;
+        if (entry.status === "error") throw entry.error;
+
+        return new Promise((resolve, reject) => {
+            entry.listeners.push({ resolve, reject });
+        });
     }
 
-    return promise;
+    const listeners: Array<StatusListener<T>> = [];
+    try {
+        cache.set(url, statusProgress(listeners));
+
+        const value = await fetchJsonUncached(url);
+        cache.set(url, statusSuccess(value));
+        listeners.forEach(({ resolve }) => resolve(value));
+
+        return value;
+    } catch (error) {
+        listeners.forEach(({ reject }) => reject(error));
+        cache.set(url, statusError(error), 20); // shorter TTL for errors
+        throw error;
+    }
 }
 
 export function fetchRAM<T>(path: string) {
