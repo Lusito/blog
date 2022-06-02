d790f3057f3e2f643da615af9d779f56638ecb3d
Santo Pfingsten <core@lusito.info>
2022-06-02 13:57:26 +0200

    categories page

--- packages/tsx-dom-ssg-demo/src/pages/site/categories.page.tsx
+++ packages/tsx-dom-ssg-demo/src/pages/site/categories.page.tsx
@@ -6,11 +6,11 @@ import { FrontMatter, tagLabels } from "../../utils/pageUtils";
 
 export const frontMatter: FrontMatter = {
     tags: [],
-    title: "File Not Found",
+    title: "All Categories",
     image: "default.jpg",
     description: "",
     date: "2022-06-01T10:44:12.865Z",
-    slug: "404",
+    slug: "categories",
 };
 
 // eslint-disable-next-line func-names
@@ -20,7 +20,7 @@ export default async function () {
             <main>
                 <ArticleHead title={frontMatter.title} image={frontMatter.image} />
                 <Container>
-                    <p>Could not find the file you were looking for! Try looking at one of these topics:</p>
+                    {/* fixme: nicer listing including tag descriptions? */}
                     <TagList tags={Object.values(tagLabels)} />
                 </Container>
             </main>
