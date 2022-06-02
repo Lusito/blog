4b52a79bdc4da0a0af3ba76e8c1a7b016bce6f02
Santo Pfingsten <core@lusito.info>
2022-06-03 00:30:18 +0200

    base tag

--- packages/tsx-dom-ssg-demo/src/layouts/DefaultLayout.tsx
+++ packages/tsx-dom-ssg-demo/src/layouts/DefaultLayout.tsx
@@ -17,6 +17,7 @@ export const DefaultLayout = withCss(classes, ({ children, pageTitle }: DefaultL
         <head>
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <base href="/" />
             <title>
                 {pageTitle} - {siteTitle}
             </title>
