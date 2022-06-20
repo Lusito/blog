bd4fd05ff55f669a65091f62f04dbe91480c5d25
Santo Pfingsten <core@lusito.info>
2022-06-20 18:47:31 +0200

    tabindex fix

--- packages/tsx-dom-ssg-demo-elements/src/elements/CodeCopyButton.ts
+++ packages/tsx-dom-ssg-demo-elements/src/elements/CodeCopyButton.ts
@@ -1,10 +1,12 @@
 class CodeCopyButton extends HTMLElement {
     connectedCallback() {
+        this.setAttribute("tabindex", "0");
         this.className = this.getAttribute("enhancedClass") ?? "";
         this.addEventListener("click", this.onClick);
     }
 
     disconnectedCallback() {
+        this.removeAttribute("tabindex");
         this.removeEventListener("click", this.onClick);
     }
 
