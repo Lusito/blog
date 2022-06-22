f19950ea74ff9ec55528a14ddd590956981b9ce8
Santo Pfingsten <core@lusito.info>
2022-06-22 20:38:49 +0200

    footer wrapping

--- packages/tsx-dom-ssg-demo/src/components/Footer/Footer.tsx
+++ packages/tsx-dom-ssg-demo/src/components/Footer/Footer.tsx
@@ -3,8 +3,9 @@ import classes from "./Footer.module.scss";
 
 export const Footer = withCss(classes, () => (
     <footer class={classes.footer}>
-        © {new Date().getFullYear()} Santo Pfingsten
-        {" - "}
-        Powered by <a href="https://github.com/lusito/tsx-dom">tsx-dom-ssr</a>
+        <span>© {new Date().getFullYear()} Santo Pfingsten</span>
+        <span>
+            Powered by <a href="https://github.com/lusito/tsx-dom">tsx-dom-ssr</a>
+        </span>
     </footer>
 ));
