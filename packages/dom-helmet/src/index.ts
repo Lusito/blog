5e29940df959df00bd5a388170e1146f20b8334a
Lusito <core@lusito.info>
2022-07-08 21:38:26 +0200

    adjusted description/readme

--- packages/dom-helmet/src/index.ts
+++ packages/dom-helmet/src/index.ts
@@ -10,13 +10,6 @@ export type HelmetOptions = {
 
 /**
  * This is a helper similar to "react-helmet".
- * It moves the children of all html > body > head elements into html > head.
- * After that it removes the false head elements.
- *
- * - Duplicates will be removed if not within the same head element:
- *   - Tags: title, base, meta, link, script (with src attribute) and any tag with an id attribute
- * - Transfers attributes from false html and body tags to their real counterparts
- *   - Classes and styles will be appended, other attributes will be replaced.
  *
  * @param elements An object containing the elements required.
  */
