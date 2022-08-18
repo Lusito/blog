2300074cd39b43a2ceac160be3d23f8491495088
Santo Pfingsten <core@lusito.info>
2022-08-18 10:36:42 +0200

    alt

--- packages/tsx-dom-ssg-demo/src/components/SideBarHeader/SideBarHeader.tsx
+++ packages/tsx-dom-ssg-demo/src/components/SideBarHeader/SideBarHeader.tsx
@@ -6,7 +6,7 @@ import { siteTitle } from "../../utils/config";
 // eslint-disable-next-line func-names
 export const SideBarHeader = withCss(classes, () => (
     <header class={classes.sideBarHeader}>
-        <img src={`/${logo}`} alt="logo" />
+        <img src={`/${logo}`} alt="My head as a vector graphic" />
         <h3>{siteTitle}</h3>
     </header>
 ));
