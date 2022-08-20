342e4b837d72aad0fd4761cac34bbeac43310e50
Santo Pfingsten <core@lusito.info>
2022-08-20 13:50:01 +0200

    favicons

--- packages/tsx-dom-ssg-demo/src/layouts/DefaultLayout.tsx
+++ packages/tsx-dom-ssg-demo/src/layouts/DefaultLayout.tsx
@@ -17,6 +17,10 @@ export const DefaultLayout = withCss(classes, ({ children, pageTitle }: DefaultL
         <head>
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png" />
+            <link rel="shortcut icon" href="/assets/favicon.ico" />
             <base href="/" />
             <title>
                 {pageTitle} - {siteTitle}
