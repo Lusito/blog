2d9f2e3dd82589e3d25cf4a1495af93b78d254aa
Santo Pfingsten <core@lusito.info>
2022-08-29 20:41:50 +0200

    synopsis plus icon

--- packages/blog/src/components/Synopsis/Synopsis.tsx
+++ packages/blog/src/components/Synopsis/Synopsis.tsx
@@ -2,9 +2,11 @@ import { BaseProps } from "tsx-dom-ssr";
 
 import { withCss } from "../../utils/withCss";
 import classes from "./Synopsis.module.scss";
+import logo from "../SideBarHeader/logo.webp";
 
 export const Synopsis = withCss(classes, ({ children }: BaseProps) => (
     <div class={classes.synopsis}>
+        <img src={`/${logo}`} alt="My head as a vector graphic" />
         <p>{children}</p>
     </div>
 ));
