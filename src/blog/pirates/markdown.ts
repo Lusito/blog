ef9e7e1264ddd1ad0522594dd82f89b16b01985d
Santo Pfingsten <core@lusito.info>
2022-11-05 13:28:07 +0100

    plugin cleanup

--- src/blog/pirates/markdown.ts
+++ src/blog/pirates/markdown.ts
@@ -1,4 +1,5 @@
 import { createMarkdownHandler } from "@lusito/require-libs";
+import MarkdownIt from "markdown-it";
 import { join } from "path";
 import { addHook } from "pirates";
 
@@ -11,15 +12,41 @@ function isFrontMatter(object: unknown): object is FrontMatter {
     return object !== null && typeof object === "object" && "image" in object;
 }
 
+function copyButtonPlugin(md: MarkdownIt) {
+    for (const rule of ["code_block", "fence"]) {
+        const original = md.renderer.rules[rule];
+        if (original) {
+            md.renderer.rules[rule] = (...args) => {
+                const [tokens, idx] = args;
+                const { content } = tokens[idx];
+                const html = original(...args);
+
+                if (content.length === 0) return html;
+
+                const title = "Copy the code snippet to the clipboard";
+                const el = createElement("code-copy-button");
+                el.setAttribute("code", content);
+                el.setAttribute("title", title);
+                el.setAttribute("aria-title", title);
+                el.setAttribute("enhancedClass", classes.enhancedCopyButton);
+
+                return html.replace("</pre>", `${el.outerHTML}</pre>`);
+            };
+        }
+    }
+}
+
 // Markdown
 const markdownHandler = createMarkdownHandler({
     copyAsset,
-    copyButtonEnhancedClass: classes.enhancedCopyButton,
     createElement,
     postProcess({ dir, frontMatter }) {
         if (isFrontMatter(frontMatter) && frontMatter.image) {
             frontMatter.image = copyAsset(join(dir, frontMatter.image));
         }
+    },
+    setup(md) {
+        md.use(copyButtonPlugin);
     },
 });
 
